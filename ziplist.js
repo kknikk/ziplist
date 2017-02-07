43d9e1b9049aaa31aa3906ed31c487ff31f61ebb
Kurt <knikaitani@gmail.com>
2017-02-06 23:20:51 -1000

    config update
    config update

--- ziplist.js
+++ ziplist.js
@@ -3,9 +3,9 @@
  * Created by knika on 2/6/2017.
  */
 
-function zipList(list1, list2){
-  let alt = [];
-  for (let i = 0; i < list1.length; i++){
+function zipList(list1, list2) {
+  const alt = [];
+  for (let i = 0; i < list1.length; i++) {
     alt.push(list1[i]);
     alt.push(list2[i]);
   }
@@ -17,7 +17,7 @@ const test1 = ['a', 'b', 'c'];
 
 console.log(zipList(test1, test2));
 
-function zipListTheSimpleWay(list1, list2){
+function zipListTheSimpleWay(list1, list2) {
   const newList = _.zip(list1, list2);
   return _.flatten(newList);
 }
